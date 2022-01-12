1a3584b2d52d9cbe7653a6d81ee09edf746ddea5
Leonid Vekselman <leonid.gaidai1989@gmail.com>
2022-01-12 14:52:28 +0200

    Test checkAppFileChanged

--- jenkins/Utils.groovy
+++ jenkins/Utils.groovy
@@ -1,11 +1,11 @@
 #!groovy
 def checkAppFileChanged(exp, branch_name){
     def GIT_FILES = sh(
-            script: 'git ls-tree --full-tree -r --name-only ' + branch_name,
+            script: "git ls-tree --full-tree -r --name-only ${branch_name}",
             returnStdout: true
     ).thim().split('\n').findAll{ it =~ exp}
     def CURRENT_COMMIT_ID = sh(
-            script: 'git show-ref --hash refs/remotes/origin/' + branch_name,
+            script: "git show-ref --hash refs/remotes/origin/ + ${branch_name}",
             returnStdout: true
     ).thim()
     for (String file in GIT_FILES){
